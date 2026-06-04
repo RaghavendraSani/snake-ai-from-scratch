a5ccd09e922dcbfd7ea9eed5a1fb1fe3a9c7513e
RaghavendraSani <raghavendrasinghsani@gmail.com>
2026-06-04 12:56:22 +0530

    Implement snake game and  RL environment foundation

---
+++ game/snake_game.py
@@ -0,0 +1,142 @@
+from operator import truediv
+
+import pygame
+import random
+
+class SnakeGame:
+    def __init__(self):
+        pygame.init()
+
+        self.WIDTH, self.HEIGHT = 600, 400
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("Snake AI")
+
+        self.clock = pygame.time.Clock()
+
+        self.snake_pos = [100, 50]
+        self.snake_body = [[100, 50]]
+        self.snake_size = 10
+
+        self.direction = "RIGHT"
+
+        self.food_pos = [random.randrange(0, self.WIDTH, 10),
+                         random.randrange(0, self.HEIGHT, 10)]
+
+        self.food_size = 10
+        self.score = 0
+        self.font = pygame.font.SysFont("Arial", 20)
+
+    def run (self):
+        running = True
+        action = 1
+        self.step(action)
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            action = 1
+            state, food, reward, done = self.step(action)
+
+            if done:
+                self.reset()
+
+            # drawing:
+
+            # screen
+            self.screen.fill((0, 0, 0))
+
+            # snake body
+            for block in self.snake_body:
+                pygame.draw.rect(self.screen, (0, 255, 0),
+                                 (block[0], block[1], self.snake_size, self.snake_size))
+
+            # food
+            pygame.draw.rect(self.screen, (255, 0, 0),
+                             (self.food_pos[0], self.food_pos[1], self.food_size, self.food_size))
+
+            # score
+            score_text = self.font.render(f"Score: {self.score}", True, (255, 255, 255))
+            self.screen.blit(score_text, (10, 10))
+
+            # update
+            pygame.display.update()
+
+            self.clock.tick(10)
+
+        pygame.quit()
+
+    def step(self, action):
+        reward = -1
+        done = False
+
+        #set direction
+        if action == 0:
+            self.direction = "LEFT"
+        elif action == 1:
+            self.direction = "RIGHT"
+        elif action == 2:
+            self.direction = "UP"
+        elif action == 3:
+            self.direction = "DOWN"
+
+        #move
+        if self.direction == "RIGHT" and self.direction != "LEFT":
+            self.snake_pos[0] += 10
+        elif self.direction == "LEFT" and self.direction != "RIGHT":
+            self.snake_pos[0] -= 10
+        elif self.direction == "UP" and self.direction != "DOWN":
+            self.snake_pos[1] -=10
+        elif self.direction == "DOWN" and self.direction != "UP":
+            self.snake_pos[1] += 10
+
+        #body update
+        self.snake_body.insert(0, list(self.snake_pos))
+        self.snake_body.pop()
+
+        #food collision
+        if self.snake_pos == self.food_pos:
+            self.snake_body.append(list(self.snake_body[-1]))
+            self.food_pos = [random.randrange(0, self.WIDTH, 10),
+                             random.randrange(0, self.HEIGHT, 10)]
+            self.score += 1
+            reward = 10
+
+        #wall collision
+        if (self.snake_pos[0] <0 or
+                self.snake_pos[0] >= self.WIDTH or
+                self.snake_pos[1] < 0 or
+                self.snake_pos[1] >= self.HEIGHT):
+            reward = -10
+            done = True
+
+        #self collision
+        for block in self.snake_body[1:]:
+            if block == self.snake_pos:
+                reward = -10
+                done = True
+
+        return self.snake_pos, self.food_pos, reward, done
+
+
+    def reset(self):
+        self.snake_pos = [100, 50]
+        self.snake_body = [[100, 50]]
+        self.direction = "RIGHT"
+
+        self.food_pos = [random.randrange(0, self.WIDTH, 10),
+                         random.randrange(0, self.HEIGHT, 10)]
+        self.score = 0
+
+    def get_state(self):
+        return self.snake_pos, self.food_pos
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from game.snake_game import SnakeGame
+
+game = SnakeGame()
+game.run()
